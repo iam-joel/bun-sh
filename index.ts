d0be898bc4cbe786ed1a5a6c64471b1e9bc9908c
Joel Saji <joelsaji@Joels-MBP.local>
2023-10-21 12:54:11 -0400

    web server with figlet

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
-const z = 10;
+import figlet from 'figlet';
 
-const read = (text: string) => {
-  return text;
-}
+const server = Bun.serve({
+  port: 3000,
+  fetch(request, server) {
+    const body = figlet.textSync("Hola!");
+    return new Response(body);
+  },
+})
 
-const text = read("Hello world!@");
-
-console.log(text);
+console.log(`Listening on PORT ${server.port}`)
