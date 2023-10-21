c128e15683240f688aed4222cc4065e42b44c85b
Joel Saji <joelsaji@Joels-MBP.local>
2023-10-21 12:58:40 -0400

    adding routes

--- index.ts
+++ index.ts
@@ -3,6 +3,19 @@ import figlet from 'figlet';
 const server = Bun.serve({
   port: 3000,
   fetch(request, server) {
+    const url =   new URL(request.url);
+    if(url.pathname == '/'){
+      const body = figlet.textSync("home");
+      return new Response(body);
+    }
+    if(url.pathname == '/about'){
+      const body = figlet.textSync("about");
+      return new Response(body);
+    }
+    if(url.pathname == '/contact'){
+      const body = figlet.textSync("contact");
+      return new Response(body);
+    }
     const body = figlet.textSync("Hola!");
     return new Response(body);
   },
